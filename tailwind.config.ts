04c952fec2e33b79a79036fcde5fea6acb0f6dbc
remiangelo <justryeoi@gmail.com>
2025-03-27 21:59:38 +0400

    font change. trust me on this

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Arial", ...fontFamily.sans],
+        sans: ["'Lexend Deca'", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
